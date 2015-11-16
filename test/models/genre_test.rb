66545247bff02aa12cc0af5e147c4833ef08b98a
Zeljko Loncar <zeljko.loncar@onthebeach.co.uk>
2015-11-16 08:52:53 +0000

    Mocha.

--- test/models/genre_test.rb
+++ test/models/genre_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class GenreTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @genre = genres(:one)
+  end
+
+  test 'test first book in the genre is finished' do
+    # Book.stubs(:finished?)
+    assert @genre.books.first.finished?
+  end
 end
